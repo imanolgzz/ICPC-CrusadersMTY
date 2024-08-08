483051ccc7aa97074811b1957d110ae3bfdeff19
Imanol González <imanolgzzsolis@gmail.com>
2024-08-08 12:32:27 -0600

    small changes

--- Codeforces/964_div4/F.cpp
+++ Codeforces/964_div4/F.cpp
@@ -5,9 +5,44 @@
 #include <math.h>
 using namespace std;
 
+const int mod = 1e9+7;
+vector<int> factorials(2e5 + 1);
+
 void setIO(){
   freopen("input.in", "r", stdin);
   freopen("output.out", "w", stdout);
+}
+
+int power(int a, int b){
+  int output = 1;
+  while(b > 0){
+    if(b&1){
+      output = ((long long)output * a)% mod;
+    }
+    a = a * a;
+    b = b >> 1;
+  }
+  return output;
+}
+
+int modularInverse(int a){
+  return power(a,mod-2);
+
+}
+
+int factorial(int num){
+  if(factorials[num] != 0){
+    return factorials[num];
+  }
+
+  factorials[num] = ((long long) num * factorial(num-1)) % mod;
+  return factorials[num];
+}
+
+int comb(int a, int b){
+  // n! / (n-r)! r!
+  int output = ((long long)((long long)factorial(a) * modularInverse(factorial(b)))% mod * modularInverse(factorial(a-b))) % mod; 
+  return output;
 }
 
 int main(){
@@ -28,6 +63,11 @@ int main(){
     }
     cout << ones << " " << ceros << endl;
     int output = 0;
+    int start = k/2 + 1;
+    for(int i = start; i <=ones; i++){
+      output =  (output + comb(k, i)) % mod;
+    }
+    cout << output << endl;
 
   }
   return 0;
